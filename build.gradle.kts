6d46dc3987760e2ecad2d4a60004dcc02b73161b
Demonstratorz <157326583+chicanoandres702@users.noreply.github.com>
2025-10-26 15:45:08 +0000

    feat: Implement issue #1 - Add/Update build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+subprojects {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = libs.versions.jvmToolchain.get()
+        }
+    }
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
